0107b4275f636598189465ef835e65605eba4036
muradorucov <orucowmurad@gmail.com>
2022-09-08 10:19:54 +0400

    blur change

--- main.js
+++ main.js
@@ -4,7 +4,30 @@ let backgroundOBJ = document.querySelector("#background-mg")
 backgroundOBJ.style.filter = "blur(20px)";
 
 password.addEventListener("keyup", (e) => {
-    var item = 20;
-    item = password.value.length / 20
-    backgroundOBJ.style.filter = `blur(${item}+ px)`;
+    switch(password.value.length){
+        case 1:
+            backgroundOBJ.style.filter = `blur(17px)`;
+            break;
+        case 2:
+            backgroundOBJ.style.filter = `blur(14px)`;
+            break;
+        case 3:
+            backgroundOBJ.style.filter = `blur(12px)`;
+            break;
+        case 4:
+            backgroundOBJ.style.filter = `blur(10px)`;
+            break;
+        case 5:
+            backgroundOBJ.style.filter = `blur(8px)`;
+            break;
+        case 6:
+            backgroundOBJ.style.filter = `blur(5px)`;
+            break;
+        case 7:
+            backgroundOBJ.style.filter = `blur(3px)`;
+            break;
+        case 8:
+            backgroundOBJ.style.filter = `blur(0px)`;
+            break;
+    }
 })
